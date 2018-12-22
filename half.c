b5921f2165b86a325ebc032d03c5ab91475bedd2
Ankan Poddar <friendyankan@gmail.com>
2018-12-22 18:02:57 +0530

    Assignment 2

---
+++ half.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+
+char* itoa(int val, int base){
+    
+    static char buf[32] = {0};    
+    int i = 30;    
+    for(; val && i ; --i, val /= base)    
+        buf[i] = "0123456789abcdef"[val % base];   
+    return &buf[i+1];
+    
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1)
+        exit(0);
+        
+    int n = atoi(argv[argc - 1]);
+    int res = n / 2;
+    printf("Halving, Current process id: %u, Current result: %d\n", getpid(), res);
+
+    char* v[argc];
+    for (int i = 1; i < (argc-1) ; i++) {
+        v[i-1] = argv[i];
+    }
+        
+    v[argc-2] = itoa(res, 10);
+    v[argc-1] = NULL;
+    
+    execvp(v[0], v);
+}
+    
